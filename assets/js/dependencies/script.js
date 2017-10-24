bd7b0ae298983a2af9cc042bd36a5da48c3a2226
Unknown <russell.w.lamb@gmail.com>
2017-10-24 15:52:45 -0400

    asdf

--- assets/js/dependencies/script.js
+++ assets/js/dependencies/script.js
@@ -1,3 +1,13 @@
+/*
+	Subscription init blocks
+	Some have delta logic of <1 hour to insure
+	we don't loose information when we disconnect
+
+*/
+//{"updatedAt": {">":"2016-01-01T00:00:00.000Z"}
+
+
+
 // JavaScript Document
  function createNotificaiton(text) {
     var html = '<div class="notification" style="display:none"><h2>'+ text +'</h2></div>';
@@ -8,6 +18,7 @@
     alert('got data:' + JSON.stringify(data));
     createNotificaiton(data.data.text);
   });
+
   io.socket.get('/notification?limit=1', function gotResponse(body, response) {
       //createNotificaiton(data.text);
       console.log(body);
