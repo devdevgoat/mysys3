69a06468ab5ce8ed233f7786d0cd7094fa0eadc1
Unknown <russell.w.lamb@gmail.com>
2017-11-02 19:29:30 -0400

    Auto stash before merge of "master" and "origin/master"

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -1,6 +1,7 @@
 //step 8: http://iliketomatoes.com/implement-passport-js-authentication-with-sails-js-0-10-2/
 module.exports = function(req, res, next) {
-   if (req.isAuthenticated && req.user) {
+   if (req.isAuthenticated && req.session.user) {
+   		console.log('player is uthenticated');
         return next();
     }
     else{
